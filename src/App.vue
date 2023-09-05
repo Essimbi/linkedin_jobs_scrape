50235de6241d162b064d5689ec4cebdcaab5aa03
Essimbi <essimbideranot@gmail.com>
2023-09-05 22:44:38 +0100

    V1 frontend

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,48 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+<script setup>
+import { ref } from 'vue';
+import axios from 'axios';
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+import ResultContainer from './components/ResultContainer.vue' ;
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+const search = ref('');
+const isLoading = ref(false) ;
+const isData = ref(false) ;
+const job = ref({}) ;
+
+const handleSearch = () => {
+  const url = "http://localhost:3000/api/scrape";
+  isLoading.value = true ;
+  axios.post(url, { search: search.value })
+    .then((response) => {
+      isLoading.value = false
+      search.value = "";
+      isData.value = true
+      job.value = response.data ;
+    })
+    .catch(error => console.log(error));
 }
+
 </script>
+
+<template>
+  <div class="container align-items-center justify-content-center body">
+    <form @submit.prevent="handleSearch">
+      <div class="container row align-items-center justify-content-center">
+        <div class="col-10">
+          <input type="text" class="form-control" v-model="search">
+        </div>
+        <button type="submit" class="btn btn-primary col-2">Collecter</button>
+      </div>
+    </form>
+    <div class="">
+      <div v-if="isLoading" class="spinner-border text-primary mt-4" role="status">
+        <span class="visually-hidden">Loading...</span>
+      </div>
+      <ResultContainer v-if="isData" :job="job"/>
+
+    </div>
+  </div>
+</template>
 
 <style>
 #app {
@@ -22,5 +52,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.body {
+  margin-top: 20%;
 }
 </style>
---
+++ src/components/ResultContainer.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps(['job']) ;
+
+</script>
+
+<template>
+    <div class="mt-4">
+        <div class="card">
+            <div class="card-header">
+                <h1 class="card-title">
+                    Résultat de la collecte
+                </h1>
+            </div>
+            <div class="card-body">
+                <h4 class="card-text"><strong>Titre de l'offre :</strong> {{ props.job.titre }} </h4>
+                <h4><strong>Date :</strong> {{ props.job.datePoste }}</h4>
+                <h4><strong>Compagnie/Entreprise :</strong> {{ props.job.entreprise }}</h4>
+                <h4><strong>Localisation :</strong> {{ props.job.location }}</h4>
+                <h4><strong>Nombre de candidats ayant postulés :</strong> {{ props.job.nbrCandidat }}</h4>
+                <hr>
+                <p>
+                    <span> <h4> <strong>Description</strong></h4></span><br>
+                    {{ props.job.description }}
+                </p>
+            </div>
+        </div>        
+    </div>
+</template>
+
+<style>
+    .card-body {
+        text-align: left;
+    }
+</style>
