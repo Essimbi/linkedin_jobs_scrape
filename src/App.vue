89dcf30690ee9145310597b7bd5fac2304758e30
Essimbi <essimbideranot@gmail.com>
2023-09-16 12:28:26 +0100

    Validation des url avant la requête

--- src/App.vue
+++ src/App.vue
@@ -2,25 +2,35 @@
 import { ref } from 'vue';
 import axios from 'axios';
 
-import ResultContainer from './components/ResultContainer.vue' ;
+import ResultContainer from './components/ResultContainer.vue';
 
 const search = ref('');
-const isLoading = ref(false) ;
-const isData = ref(false) ;
-const job = ref({}) ;
+const isLoading = ref(false);
+const isData = ref(false);
+const isValid = ref(true);
+const job = ref({});
 
 const handleSearch = () => {
-  const url = "http://localhost:3000/api/scrape";
-  isLoading.value = true ;
-  isData.value = false ;
-  axios.post(url, { search: search.value })
-    .then((response) => {
-      isLoading.value = false
-      search.value = "";
-      isData.value = true
-      job.value = response.data ;
-    })
-    .catch(error => console.log(error));
+  const urlApi = "http://localhost:3000/api/scrape";
+
+  const urlRegex = /^https:\/\/www\.linkedin\.com\/jobs\/view\/\d+\/$/
+
+  if (urlRegex.test(search.value)) {
+    isValid.value = true
+    isLoading.value = true;
+    isData.value = false;
+    axios.post(urlApi, { search: search.value })
+      .then((response) => {
+        isLoading.value = false
+        search.value = ""
+        isData.value = true
+        job.value = response.data
+      })
+      .catch(error => console.log(error))
+  } else {
+    isValid.value = false
+    search.value = "";
+  }
 }
 
 </script>
@@ -30,7 +40,7 @@ const handleSearch = () => {
     <form @submit.prevent="handleSearch">
       <div class="container row align-items-center justify-content-center">
         <div class="col-10">
-          <input type="text" class="form-control" v-model="search">
+          <input type="text" class="form-control" v-model="search" placeholder="Saisissez l'url du du job">
         </div>
         <button type="submit" class="btn btn-primary col-2">Collecter</button>
       </div>
@@ -39,7 +49,11 @@ const handleSearch = () => {
       <div v-if="isLoading" class="spinner-border text-primary mt-4" role="status">
         <span class="visually-hidden">Loading...</span>
       </div>
-      <ResultContainer v-if="isData" :job="job"/>
+      <ResultContainer v-if="isData" :job="job" />
+      <div class="container mt-4" v-if="!isValid">
+        <p>Url non valide, utilisez une url sous la forme <strong>https://www.linkedin.com/jobs/view/3709823773/</strong>
+        </p>
+      </div>
 
     </div>
   </div>
